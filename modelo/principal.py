97b654caff4f0df564b1f6dcffdd278d3c9942f2
Jenifer Castaño Ledesma <jennifercasled@gmail.com>
2024-03-29 15:44:16 -0500

    update interface's images

--- modelo/principal.py
+++ modelo/principal.py
@@ -5,7 +5,7 @@ import sys
 from pygame.locals import *
 
 # Leer el archivo de texto y convertirlo en una matriz
-matriz = np.loadtxt('../matrices/matriz1.txt', dtype=int)
+matriz = np.loadtxt('./matrices/matriz1.txt', dtype=int)
 
 #  print(matriz[0][0])
 
@@ -31,35 +31,33 @@ ALTO = 700
 PANTALLA = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption('El Ninja')
 
-ninja_stop = pygame.transform.scale(pygame.image.load('../imagenes/ninja-stop.jpg'), (60, 60))
+ninja_stop = pygame.transform.scale(pygame.image.load('./imagenes/ninja-stop.jpg'), (60, 60))
 # icono del juego
 pygame.display.set_icon(ninja_stop)
 
-caminaDerecha = [pygame.transform.scale(pygame.image.load('../imagenes/ninjas-right1.jpg'), (60, 60)),
-                 pygame.transform.scale(pygame.image.load('../imagenes/ninjas-right2.jpg'), (60, 60)),
-                 pygame.transform.scale(pygame.image.load('../imagenes/ninjas-right3.jpg'), (60, 60))]
+caminaDerecha = [pygame.transform.scale(pygame.image.load('./imagenes/ninjas-right1.jpg'), (60, 60)),
+                 pygame.transform.scale(pygame.image.load('./imagenes/ninjas-right2.jpg'), (60, 60)),
+                 pygame.transform.scale(pygame.image.load('./imagenes/ninjas-right3.jpg'), (60, 60))]
 
-caminaIzquierda = [pygame.transform.scale(pygame.image.load('../imagenes/ninjas-left1.jpg'), (60, 60)),
-                   pygame.transform.scale(pygame.image.load('../imagenes/ninjas-left2.jpg'), (60, 60)),
-                   pygame.transform.scale(pygame.image.load('../imagenes/ninjas-left3.jpg'), (60, 60))]
+caminaIzquierda = [pygame.transform.scale(pygame.image.load('./imagenes/ninjas-left1.jpg'), (60, 60)),
+                   pygame.transform.scale(pygame.image.load('./imagenes/ninjas-left2.jpg'), (60, 60)),
+                   pygame.transform.scale(pygame.image.load('./imagenes/ninjas-left3.jpg'), (60, 60))]
 
-shuriken = pygame.transform.scale(pygame.image.load('../imagenes/shuriken.png'), (60, 60))
+shuriken = pygame.transform.scale(pygame.image.load('./imagenes/shuriken.png'), (60, 60))
 
-espacioVacio = pygame.transform.scale(pygame.image.load('../imagenes/espacio-vacio.png'), (60, 60))
-paisaje = pygame.transform.scale(pygame.image.load('../imagenes/paisajeNinja1.jpg'), (600, 600))
-espacioVacio.set_colorkey(BLANCO)
-muro5 = pygame.transform.scale(pygame.image.load('../imagenes/muro5.jpg'), (60, 60))
-muro2 = pygame.transform.scale(pygame.image.load('../imagenes/muro2.jpg'), (60, 60))
-espacioVacioBottom = pygame.transform.scale(pygame.image.load('../imagenes/espacio-vacio-bottom.png'), (60, 60))
-espacioVacioTop = pygame.transform.scale(pygame.image.load('../imagenes/espacio-vacio-top.png'), (60, 60))
-espacioVacioLeft = pygame.transform.scale(pygame.image.load('../imagenes/espacio-vacio-left.png'), (60, 60))
-espacioVacioRight = pygame.transform.scale(pygame.image.load('../imagenes/espacio-vacio-right.png'), (60, 60))
+paisaje = pygame.transform.scale(pygame.image.load('./imagenes/paisajeNinja.jpg'), (600, 600))
+
+muro2 = pygame.transform.scale(pygame.image.load('./imagenes/muro2.jpg'), (60, 60))
+espacioVacioBottom = pygame.transform.scale(pygame.image.load('./imagenes/espacio-vacio-bottom.png'), (60, 60))
+espacioVacioTop = pygame.transform.scale(pygame.image.load('./imagenes/espacio-vacio-top.png'), (60, 60))
+espacioVacioLeft = pygame.transform.scale(pygame.image.load('./imagenes/espacio-vacio-left.png'), (60, 60))
+espacioVacioRight = pygame.transform.scale(pygame.image.load('./imagenes/espacio-vacio-right.png'), (60, 60))
 espacioVacioLeft.set_colorkey(BLANCO)
 espacioVacioRight.set_colorkey(BLANCO)
 espacioVacioTop.set_colorkey(BLANCO)
 espacioVacioBottom.set_colorkey(BLANCO)
 
-espacioVacio2 = pygame.transform.scale(pygame.image.load('../imagenes/espacio-vacio2.png'), (60, 60))
+espacioVacio2 = pygame.transform.scale(pygame.image.load('./imagenes/espacio-vacio.png'), (60, 60))
 espacioVacio2.set_colorkey(BLANCO)
 
 imagenes = {0: muro2,1: espacioVacio2,
